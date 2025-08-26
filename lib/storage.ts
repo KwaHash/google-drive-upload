cb8659544aac03dec27cb1c05e5376a522eac435
StackNinja0109 <whitecrow0109@gmail.com>
2025-08-26 11:24:29 +0900

    refactor: remove session and queue in storage

--- lib/storage.ts
+++ lib/storage.ts
@@ -1,10 +1,7 @@
-import { type UploadSession, type UploadQueueItem } from './types'
 
 class StorageManager {
   private static ACCESS_TOKEN_KEY = 'google_drive_access_token'
   private static REFRESH_TOKEN_KEY = 'google_drive_refresh_token'
-  private static UPLOAD_SESSION_KEY = 'upload_session'
-  private static UPLOAD_QUEUE_KEY = 'upload_queue'
 
   static getAccessToken(): string | null {
     return localStorage.getItem(this.ACCESS_TOKEN_KEY)
@@ -25,32 +22,6 @@ class StorageManager {
   static clearAuth(): void {
     localStorage.removeItem(this.ACCESS_TOKEN_KEY)
     localStorage.removeItem(this.REFRESH_TOKEN_KEY)
-  }
-
-  // Upload session management
-  static setUploadSession(session: UploadSession): void {
-    localStorage.setItem(this.UPLOAD_SESSION_KEY, JSON.stringify(session))
-  }
-
-  static getUploadSession(): UploadSession | null {
-    const data = localStorage.getItem(this.UPLOAD_SESSION_KEY)
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return data ? JSON.parse(data) : null
-  }
-
-  static setUploadQueue(queue: UploadQueueItem[]): void {
-    localStorage.setItem(this.UPLOAD_QUEUE_KEY, JSON.stringify(queue))
-  }
-
-  static getUploadQueue(): UploadQueueItem[] {
-    const data = localStorage.getItem(this.UPLOAD_QUEUE_KEY)
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return data ? JSON.parse(data) : []
-  }
-
-  static clearUploadData(): void {
-    localStorage.removeItem(this.UPLOAD_SESSION_KEY)
-    localStorage.removeItem(this.UPLOAD_QUEUE_KEY)
   }
 }
 
